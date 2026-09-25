fa4785312a684aaa88dd4094edbbbd8530501d6a
Prachee Gupta <43838606+guptaprachee@users.noreply.github.com>
2026-09-25 12:53:31 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d={')':'(', ']':'[','}':'{'}
+        n=len(s)
+        i=0
+        
+        cur= []
+        for i in range(n):
+            if s[i] in d :
+                if d[s[i]] in cur and cur[-1] == d[s[i]]:
+                    cur.pop()
+                else:
+                    return False
+            else:
+                cur.append(s[i])
+            
+        return len(cur) ==0
+        
